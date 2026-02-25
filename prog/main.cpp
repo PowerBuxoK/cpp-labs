1a850e6fe07f8f4a970cdadd54e412cc5222626b
Kirill A. Gerasimenko <kag36@tpu.ru>
2026-02-25 15:39:37 +0700

    Finished task 2 in block 1

--- prog/main.cpp
+++ prog/main.cpp
@@ -1,6 +1,6 @@
-#include "LongInt.h"
 #include <cinttypes>
 #include <cstdint>
+#include <cstring>
 #include <iostream>
 
 void PrintHelp(char* argv[])
@@ -17,74 +17,100 @@ Tasks:
             << std::endl;
 }
 
-void Task1()
+bool ValidateIP(const std::string& input_ip)
 {
-  uint64_t n = 0;
-  std::cout << "Input number to calculate factorial from: " << std::endl;
-  std::cin >> n;
-  std::cout << "Factorial of " << n << " is " << fact(n) << std::endl;
+  // Check wether the IP follows a %d.%d.%d.%d template
+  int dot_count = 0;
+  for(size_t i = 0; i < input_ip.size(); i++)
+  {
+    char c = input_ip[i];
+    if(c == '.' && (i + 1) < input_ip.size())
+    {
+      dot_count++;
+    }
+    else if(!std::isdigit(c))
+    {
+      std::cout << "IP malformed!" << std::endl;
+      return false;
+    }
+  }
+
+  if(dot_count != 3)
+  {
+    std::cout << "IP malformed! (Dot count not equal to 3)" << std::endl;
+    return false;
+  }
+
+  {
+    uint16_t output[4] = { 0 };
+    sscanf(input_ip.c_str(), "%d.%d.%d.%d", &output[0], &output[1], &output[2], &output[3]);
+    for(size_t i = 0; i < sizeof(output) / sizeof(*output); i++)
+    {
+      if(output[i] > 255)
+      {
+        std::cout << "IP malformed! (Component " << i + 1 << " greater than 255)" << std::endl;
+        return false;
+      }
+    }
+  }
+
+  return true;
 }
 
-void Task2()
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to)
 {
-  uint64_t n = 0;
-  std::cout << "Input number to calculate 2^number from: " << std::endl;
-  std::cin >> n;
-  std::cout << "2^" << n << " is " << powered2(n) << std::endl;
+  size_t start_pos = 0;
+  while((start_pos = str.find(from, start_pos)) != std::string::npos)
+  {
+    str.replace(start_pos, from.length(), to);
+    start_pos += to.length();
+  }
+  return str;
 }
 
-void Task3()
+void Block1Task2()
 {
-  uint64_t n1 = 0, n2 = 0;
-  std::cout << "Input n1 and n2 to calculate 2^n1 + 2^n2: " << std::endl;
-  std::cin >> n1 >> n2;
-  std::cout << "2^" << n1 << "+2^" << n2 << " is " << (powered2(n1) + powered2(n2)) << std::endl;
-}
+  std::string input_ip;
+  std::cout << "Input a IPv4 IP:" << std::endl;
+  std::cin >> input_ip;
 
-void Task4()
-{
-  uint64_t n1 = 0, n2 = 0;
-  std::cout << "Input n1 and n2 to calculate 2^n1 - 2^n2: " << std::endl;
-  std::cin >> n1 >> n2;
-  std::cout << "2^" << n1 << "-2^" << n2 << " is " << (powered2(n1) - powered2(n2)) << std::endl;
-}
+  if(!ValidateIP(input_ip))
+  {
+    return;
+  }
 
-void Task5()
-{
-  uint64_t n = 0;
-  std::cout << "Input number to calculate fibonacci from: " << std::endl;
-  std::cin >> n;
-  std::cout << "Fibonacci of " << n << " is " << fibonacci(n) << std::endl;
+  std::cout << "Replaced . with \"[.]\"" << ReplaceAll(input_ip, ".", "\"[.]\"") << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
-  if(argc <= 1)
-  {
-    PrintHelp(argv);
-    return 0;
-  }
+  // if(argc <= 1)
+  // {
+  //   PrintHelp(argv);
+  //   return 0;
+  // }
 
-  switch(argv[1][0])
-  {
-    case '1':
-      Task1();
-      break;
-    case '2':
-      Task2();
-      break;
-    case '3':
-      Task3();
-      break;
-    case '4':
-      Task4();
-      break;
-    case '5':
-      Task5();
-      break;
-    default:
-      std::cout << "Unknown usage!" << std::endl;
-      break;
-  }
+  Block1Task2();
+  // switch(argv[1][0])
+  // {
+  //   case '1':
+  //     Task1();
+  //     break;
+  //   case '2':
+  //     Task2();
+  //     break;
+  //   case '3':
+  //     Task3();
+  //     break;
+  //   case '4':
+  //     Task4();
+  //     break;
+  //   case '5':
+  //     Task5();
+  //     break;
+  //   default:
+  //     std::cout << "Unknown usage!" << std::endl;
+  //     break;
+  // }
   return 0;
 }
