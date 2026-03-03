1120424310f8c45c6059b5a7b85fbd8493e0e06e
Kirill A. Gerasimenko <kag36@tpu.ru>
2026-03-03 11:24:23 +0700

    Finish lab 3

--- prog/main.cpp
+++ prog/main.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <cstring>
 #include <iostream>
+#include <vector>
 
 void PrintHelp(char* argv[])
 {
@@ -9,11 +10,9 @@ void PrintHelp(char* argv[])
             << argv[0] << R"( [task]
 
 Tasks:
-1: factorial
-2: 2^number
-3: 2^n1 + 2^n2
-4: 2^n1 - 2^n2
-5: fibonacci)"
+1: Replace . to [.] in IP
+2: Palandrome detection
+3: Letter counting)"
             << std::endl;
 }
 
@@ -21,12 +20,18 @@ bool ValidateIP(const std::string& input_ip)
 {
   // Check wether the IP follows a %d.%d.%d.%d template
   int dot_count = 0;
+  size_t last_dot_pos = 0;
   for(size_t i = 0; i < input_ip.size(); i++)
   {
     char c = input_ip[i];
+    if (c == '.' && last_dot_pos + 1 == i) {
+      std::cout << "IP malformed! (Number " << dot_count + 1 << " is empty)" << std::endl;
+      return false;
+    }
     if(c == '.' && (i + 1) < input_ip.size())
     {
       dot_count++;
+      last_dot_pos = i;
     }
     else if(!std::isdigit(c))
     {
@@ -79,10 +84,11 @@ void Block1Task2()
     return;
   }
 
-  std::cout << "Replaced . with \"[.]\"" << ReplaceAll(input_ip, ".", "\"[.]\"") << std::endl;
+  std::cout << "Replaced: " << ReplaceAll(input_ip, ".", "\"[.]\"") << std::endl;
 }
 
-bool IsPalandrome(const std::string& cs) {
+bool IsPalandrome(const std::string& cs)
+{
   // We dont need to check the middle letter
   // Because it has no effect on the result
   // The middle letter is excluded at the division
@@ -90,9 +96,9 @@ bool IsPalandrome(const std::string& cs) {
   // string size = 5
   // After division we have 2, not including
   // the middle char
-  for (size_t i = 0; i < cs.size()/2; i++)
+  for(size_t i = 0; i < cs.size() / 2; i++)
   {
-    if (cs[i]!=cs[cs.size()-i-1])
+    if(cs[i] != cs[cs.size() - i - 1])
     {
       return false;
     }
@@ -103,40 +109,89 @@ bool IsPalandrome(const std::string& cs) {
 void Block2Task1()
 {
   std::string input;
-  std::getline(std::cin,input);
-  std::string nwv = ReplaceAll(input," ","");
+  std::getline(std::cin, input);
+  std::string nwv = ReplaceAll(input, " ", "");
   std::cout << "The sentance \"" << input << "\" " << (IsPalandrome(nwv) ? "is a palandrome" : "is not a palandrome") << std::endl;
+}
+
+std::vector<int> Block3Function(const std::string& words, const std::vector<char>& letters)
+{
+  std::vector<int> score(26);
+
+  // Search string words for every letter in vector letters
+  for(char to_count : letters)
+  {
+    for(char c : words)
+    {
+      if(!std::isalpha(c))
+      {
+        std::cout << "Character '" << c << "' will be ignored" << std::endl;
+        continue;
+      }
+      if(c != to_count)
+        continue;
+      char c_lower = std::tolower(c);
+      score[c_lower - 'a']++;
+    }
+  }
+
+  return score;
+}
+
+void Block3()
+{
+  std::string words;
+  std::vector<char> letters;
+
+  std::cout << "Input letters to count" << std::endl;
+  {
+    std::string ltr_str;
+    std::cin >> ltr_str;
+    for(char& c : ltr_str)
+    {
+      if(!std::isalpha(c))
+      {
+        std::cout << "The string should only contain letters char '" << c << "' will be ignored" << std::endl;
+        continue;
+      }
+      letters.emplace_back(c);
+    }
+  }
+
+  std::cout << "Words to count in" << std::endl;
+  std::cin.ignore();
+  getline(std::cin, words);
+
+  auto score = Block3Function(words, letters);
+
+  for(char i = 0; i < score.size(); i++)
+  {
+    std::cout << (char)('a' + i) << ": " << score[i] << std::endl;
+  }
 }
 
 int main(int argc, char* argv[])
 {
-  // if(argc <= 1)
-  // {
-  //   PrintHelp(argv);
-  //   return 0;
-  // }
+  if(argc <= 1)
+  {
+    PrintHelp(argv);
+    return 0;
+  }
 
-  Block2Task1();
-  // switch(argv[1][0])
-  // {
-  //   case '1':
-  //     Task1();
-  //     break;
-  //   case '2':
-  //     Task2();
-  //     break;
-  //   case '3':
-  //     Task3();
-  //     break;
-  //   case '4':
-  //     Task4();
-  //     break;
-  //   case '5':
-  //     Task5();
-  //     break;
-  //   default:
-  //     std::cout << "Unknown usage!" << std::endl;
-  //     break;
-  // }
+  switch(argv[1][0])
+  {
+    case '1':
+      Block1Task2();
+      break;
+    case '2':
+      Block2Task1();
+      break;
+    case '3':
+      Block3();
+      break;
+    default:
+      std::cout << "Unknown usage!" << std::endl;
+      break;
+  }
   return 0;
 }
