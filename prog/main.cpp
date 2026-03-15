91efa3989c25619943a59eef29d5dcd1454e9840
PowerBuxoK <fffdghffghff@gmail.com>
2026-03-15 16:20:19 +0700

    Finish code

--- prog/main.cpp
+++ prog/main.cpp
@@ -1,197 +1,29 @@
-#include <cinttypes>
-#include <cstdint>
-#include <cstring>
-#include <iostream>
-#include <vector>
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/hal/interface.h>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 
-void PrintHelp(char* argv[])
+void sepia(cv::Mat img)
 {
-  std::cout << "Program usage: \n"
-            << argv[0] << R"( [task]
-
-Tasks:
-1: Replace . to [.] in IP
-2: Palandrome detection
-3: Letter counting)"
-            << std::endl;
-}
-
-bool ValidateIP(const std::string& input_ip)
-{
-  // Check whether the IP follows a %d.%d.%d.%d template
-  int dot_count = 0;
-  size_t last_dot_pos = 0;
-  for(size_t i = 0; i < input_ip.size(); i++)
-  {
-    char c = input_ip[i];
-    if (c == '.' && last_dot_pos + 1 == i) {
-      std::cout << "IP malformed! (Number " << dot_count + 1 << " is empty)" << std::endl;
-      return false;
-    }
-    if(c == '.' && (i + 1) < input_ip.size())
-    {
-      dot_count++;
-      last_dot_pos = i;
-    }
-    else if(!std::isdigit(c))
-    {
-      std::cout << "IP malformed!" << std::endl;
-      return false;
-    }
-  }
-
-  if(dot_count != 3)
-  {
-    std::cout << "IP malformed! (Dot count not equal to 3)" << std::endl;
-    return false;
-  }
-
-  {
-    uint16_t output[4] = { 0 };
-    sscanf(input_ip.c_str(), "%d.%d.%d.%d", &output[0], &output[1], &output[2], &output[3]);
-    for(size_t i = 0; i < sizeof(output) / sizeof(*output); i++)
-    {
-      if(output[i] > 255)
-      {
-        std::cout << "IP malformed! (Component " << i + 1 << " greater than 255)" << std::endl;
-        return false;
-      }
-    }
-  }
-
-  return true;
-}
-
-std::string ReplaceAll(std::string str, const std::string& from, const std::string& to)
-{
-  size_t start_pos = 0;
-  while((start_pos = str.find(from, start_pos)) != std::string::npos)
-  {
-    str.replace(start_pos, from.length(), to);
-    start_pos += to.length();
-  }
-  return str;
-}
-
-void Block1Task2()
-{
-  std::string input_ip;
-  std::cout << "Input a IPv4 IP:" << std::endl;
-  std::cin >> input_ip;
-
-  if(!ValidateIP(input_ip))
-  {
-    return;
-  }
-
-  std::cout << "Replaced: " << ReplaceAll(input_ip, ".", "\"[.]\"") << std::endl;
-}
-
-bool IsPalandrome(const std::string& cs)
-{
-  // We dont need to check the middle letter
-  // Because it has no effect on the result
-  // The middle letter is excluded at the division
-  // of string size by two. For example:
-  // string size = 5
-  // After division we have 2, not including
-  // the middle char
-  for(size_t i = 0; i < cs.size() / 2; i++)
-  {
-    if(cs[i] != cs[cs.size() - i - 1])
-    {
-      return false;
-    }
-  }
-  return true;
-}
-
-void Block2Task1()
-{
-  std::string input;
-  std::getline(std::cin, input);
-  std::string nwv = ReplaceAll(input, " ", "");
-  std::cout << "The sentance \"" << input << "\" " << (IsPalandrome(nwv) ? "is a palandrome" : "is not a palandrome") << std::endl;
-}
-
-std::vector<int> Block3Function(const std::string& words, const std::vector<char>& letters)
-{
-  std::vector<int> score(26);
-
-  // Search string words for every letter in vector letters
-  for(char to_count : letters)
-  {
-    for(char c : words)
-    {
-      if(!std::isalpha(c))
-      {
-        //std::cout << "Character '" << c << "' will be ignored" << std::endl;
-        continue;
-      }
-      if(c != to_count)
-        continue;
-      char c_lower = std::tolower(c);
-      score[c_lower - 'a']++;
-    }
-  }
-
-  return score;
-}
-
-void Block3()
-{
-  std::string words;
-  std::vector<char> letters;
-
-  std::cout << "Input letters to count" << std::endl;
-  {
-    std::string ltr_str;
-    std::cin >> ltr_str;
-    for(char& c : ltr_str)
-    {
-      if(!std::isalpha(c))
-      {
-        std::cout << "The string should only contain letters; char '" << c << "' will be ignored" << std::endl;
-        continue;
-      }
-      letters.emplace_back(c);
-    }
-  }
-
-  std::cout << "Words to count in" << std::endl;
-  std::cin.ignore();
-  getline(std::cin, words);
-
-  auto score = Block3Function(words, letters);
-
-  for(char i = 0; i < score.size(); i++)
-  {
-    std::cout << (char)('a' + i) << ": " << score[i] << std::endl;
-  }
+  using namespace cv;
+  Mat res = img.clone();
+  cvtColor(res, res, COLOR_BGR2RGB);
+  transform(res, res, Matx33f(0.393, 0.769, 0.189, 0.349, 0.686, 0.168, 0.272, 0.534, 0.131));
+  cvtColor(res, res, COLOR_RGB2BGR);
+  imshow("original", img);
+  imshow("Sepia", res);
+  waitKey(0);
+  destroyAllWindows();
 }
 
 int main(int argc, char* argv[])
 {
-  if(argc <= 1)
-  {
-    PrintHelp(argv);
-    return 0;
-  }
+  cv::Mat image = cv::imread("./image.png");
+  cv::resize(image, image, cv::Size(960, 540), 0, 0, cv::INTER_LINEAR);
 
-  switch(argv[1][0])
-  {
-    case '1':
-      Block1Task2();
-      break;
-    case '2':
-      Block2Task1();
-      break;
-    case '3':
-      Block3();
-      break;
-    default:
-      std::cout << "Unknown usage!" << std::endl;
-      break;
-  }
+  sepia(image);
   return 0;
 }
