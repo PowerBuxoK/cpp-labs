b72cf4c053237ecabb79997e3fb952409fbbc341
Kirill A. Gerasimenko <kag36@tpu.ru>
2026-03-17 10:24:40 +0700

    lab 5 bru

--- prog/main.cpp
+++ prog/main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
+// Add overflow check
+
 int signed_division(int a, int b)
 {
-  int quotient;
+  int result;
   asm (
       "movl %1, %%eax\n\t"    // Move dividend into EAX
       "cdq\n\t"               // Sign-extend EAX into EDX (EDX:EAX = dividend)
       "idivl %2\n\t"          // Signed divide by divisor
-      "movl %%eax, %0\n\t"    // Move quotient from EAX to output variable
-      : "=r"(quotient)        // Output operands
+      "movl %%eax, %0\n\t"    // Move result from EAX to output variable
+      : "=r"(result)          // Output operands
       : "r"(a), "r"(b)        // Input operands
       : "%eax", "%edx", "cc"  // Clobbered registers and condition codes
   );
-  return quotient;
+  return result;
 }
 
 int main(int argc, char const* argv[])
